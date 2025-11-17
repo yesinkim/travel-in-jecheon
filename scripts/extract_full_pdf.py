bff29422d9b894b7b467ce6b2478d952c8eb5d14
yesinkim <bailando.ys@gmail.com>
2025-11-18 03:44:50 +0900

    feat: Add Jecheon tourism guide markdown files and PDF extraction script
    - Created two markdown files for the Jecheon tourism guide, detailing travel information, attractions, and local experiences.
    - Implemented a new script to extract content from the Jecheon tourism PDF into markdown format for easier accessibility and editing.
    - Removed outdated Jupyter notebook and JSON statistics files related to PDF analysis to streamline the project structure.

---
+++ scripts/extract_full_pdf.py
@@ -0,0 +1,26 @@
+from dotenv import load_dotenv
+import asyncio
+from pyzerox import zerox
+
+load_dotenv()    
+pdf_path = "data/raw/제천시관광정보책자.pdf"
+
+model = "gpt-4o"
+custom_system_prompt = """
+Convert the following PDF page to markdown format.
+Allowed only extract the all content text from the PDF page. 
+You should not make any changes to the text.
+"""
+async def main():
+    output_dir = "data/processed"  ## 통합된 마크다운 파일을 저장할 디렉토리
+    result = await zerox(
+        file_path=pdf_path,
+        model=model,
+        output_dir=output_dir,
+        custom_system_prompt=custom_system_prompt,
+    )
+    return result
+
+
+# 메인 함수를 실행합니다:
+result = asyncio.run(main())
